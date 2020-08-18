567d2b723dd57bdeb8bf75db8871276d2af6b002
farhadbhuiyan <farhadbhuiyan.cse@gmail.com>
2020-08-18 21:52:08 +0600

    javascript file added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+const userName = document.getElementById('user-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('confirm-password');
+
+
+function checkInput{
+  const userNameValue = userName.Value;
+  const emailValue = email.Value;
+  const passwordValue = password.Value;
+  const passwordConfirmValue = passwordConfirm.Value;
+}
